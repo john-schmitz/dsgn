c2739ef6f0d081294076f88dd5b24112fba0ecd0
wvxbs <gabriel.ferreira7854@gmail.com>
2018-08-01 11:18:04 -0300

    muitas coisas na moral

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function DarkTheme(){
+    document.getElementById('body').style.backgroundColor = grey;
+}
+
+function LightTheme(){
+    document.getElementById('body').style.backgroundColor = white;
+}
