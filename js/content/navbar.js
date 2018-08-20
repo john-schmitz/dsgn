3d13edc40f535f70ecab75895fbc80df432905ca
Gabriel Ferreira <gabriel.ferreira7854@gmail.com>
2018-08-20 20:09:01 -0300

    navbar fixa

--- js/content/navbar.js
+++ js/content/navbar.js
@@ -1,5 +1,5 @@
 Vue.component('navbar', {
-    template: ` <nav class="navbar navbar-expand-lg navbar-dark">
+    template: ` <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
     <a class="navbar-brand text-light">Logo</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
