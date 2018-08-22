c7cc4fb6a989de104b098d3529aaa51c55c1bae2
wvxbs <gabriel.ferreira7854@gmail.com>
2018-08-21 22:53:42 -0300

    arrumando mais

--- js/content/comum.js
+++ js/content/comum.js
@@ -1,9 +1,6 @@
 Vue.component('navbar', {
   template: `<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
   <a class="navbar-brand text-light my-auto"><img class="navbar-icon" src="../imagens/logo/logo-test.png">DSGN</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav" aria-controls="navbarNav" aria-expanded="false"
-      aria-label="Toggle navigation">
-  </button>
   <ul class="navbar-nav ml-auto justify-content-center align-items-center">
       <li class="nav-item active">
           <a class="nav-link font-weight-bold" href="../html/busca.html">
@@ -17,19 +14,21 @@ Vue.component('navbar', {
 Vue.component('adm-navbar', {
     template: ` <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <a class="navbar-brand text-light my-auto"><img class="navbar-icon" src="../imagens/logo/logo-test.png">DSGN</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav" aria-controls="navbarNav" aria-expanded="false"
-        aria-label="Toggle navigation">
+    <button class="navbar-toggler my-2" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
     </button>
-    <ul class="navbar-nav ml-auto justify-content-center align-items-center">
-      <li class="nav-item active">
-          <button class="btn btn-outline-light mx-1">Ocultar Portifolio</button>
-      </li>  
-      <li class="nav-item">
-        <a class="nav-link font-weight-bold" href="../html/busca.html">
-          Pesquisar
-        </a>
-      </li>
-    </ul>
+    <div class="colapse navbar-colapse ml-auto" id="navbar">
+        <ul class="navbar-nav justify-content-center align-items-center">
+            <li class="nav-item active">
+                <button class="btn btn-outline-light mx-1">Ocultar Portifolio</button>
+            </li>  
+        <li class="nav-item">
+            <a class="nav-link font-weight-bold" href="../html/busca.html">
+            Pesquisar
+            </a>
+        </li>
+        </ul>
+    </div>
   </nav>` 
 });
 
