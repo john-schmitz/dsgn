032425b50752d267768d408cf47480a10a8d17be
wvxbs <gabriel.ferreira7854@gmail.com>
2018-08-21 21:53:12 -0300

    adição do navbar do adm

--- js/content/comum.js
+++ js/content/comum.js
@@ -14,6 +14,25 @@ Vue.component('navbar', {
 </nav>`
 });
 
+Vue.component('adm-navbar', {
+    template: ` <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand text-light my-auto"><img class="navbar-icon" src="../imagens/logo/logo-test.png">DSGN</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav" aria-controls="navbarNav" aria-expanded="false"
+        aria-label="Toggle navigation">
+    </button>
+    <ul class="navbar-nav ml-auto justify-content-center align-items-center">
+      <li class="nav-item active">
+          <button class="btn btn-outline-light mx-1">Ocultar Portifolio</button>
+      </li>  
+      <li class="nav-item">
+        <a class="nav-link font-weight-bold" href="../html/busca.html">
+          Pesquisar
+        </a>
+      </li>
+    </ul>
+  </nav>` 
+});
+
 new Vue({
     el: 'header'
 });
