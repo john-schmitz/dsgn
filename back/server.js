fc770f704d2414b9280e66a4c4a02d38473212df
Treeek <johngc2010@hotmail.com>
2018-08-29 11:38:07 -0300

    só a nata

--- back/server.js
+++ back/server.js
@@ -4,13 +4,6 @@ var path = require('path')
 var express = require('express')
 var app = express();
 app.use(express.static('../public'))
-app.use(bodyParser.json())
 const porta = 5500
-const caminhoHtml = path.join(__dirname, "..", 'docs', 'html')
-
-//roteamento dos html
-app.get("/", function (req, res) {
-    res.sendFile(path.join(caminhoHtml, '..', "index.html"))
-})
 app.listen(5500)
 console.log("Servidor rodando na porta " + porta);
