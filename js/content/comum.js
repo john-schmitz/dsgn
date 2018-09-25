28cb0292015482aaa0a269c337668c2612ec46ab
wvxbs <gabriel.ferreira7854@gmail.com>
2018-09-25 15:09:43 -0300

    busca alterada, cores alteradas, e nav alterado

--- js/content/comum.js
+++ js/content/comum.js
@@ -1,19 +1,19 @@
 Vue.component('navbar', {
-  template: `<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
-  <a class="navbar-brand text-light my-auto"><img class="navbar-icon" src="../imagens/logo/logo-test.png">DSGN</a>
+  template: `<nav class="navbar navbar-expand-lg navbar-dark fixed-top shadow-sm">
+  <a class="navbar-brand my-auto"><div class="navbar-icon"><img class="navbar-icon" src="../imagens/logo/logo-test.png"></a>
   <ul class="navbar-nav ml-auto justify-content-center align-items-center">
       <li class="nav-item active">
-          <a class="nav-link font-weight-bold" href="../html/busca.html">
-              Pesquisar
-          </a>
+          <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modal-buscar">
+          Pesquisar
+        </button>
       </li>
   </ul>
 </nav>`
 });
 
 Vue.component('adm-navbar', {
-    template: `<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand text-light my-auto"><img class="navbar-icon" src="../imagens/logo/logo-test.png">DSGN</a>
+    template: `<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm">
+    <a class="navbar-brand my-auto"><img class="navbar-icon" src="../imagens/logo/logo-test.png">DSGN</a>
     <button class="navbar-toggler my-2" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
     </button>
@@ -35,6 +35,8 @@ Vue.component('adm-navbar', {
 new Vue({
     el: 'header'
 });
+
+
 
 
 
