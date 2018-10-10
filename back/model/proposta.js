5d5359e903230ec1ad7b610ec089555bad8c7172
Treeek <johngc2010@hotmail.com>
2018-10-10 10:25:18 -0300

    ADD MODEL PROPOSTA

---
+++ back/model/proposta.js
@@ -0,0 +1,27 @@
+"use strict"
+//TO-DO: Associação
+module.exports = (sequelize, DataTypes) => {
+
+    const Proposta = sequelize.define('proposta', {
+        titulo: {
+            type: DataTypes.STRING(45),
+            allowNull: false
+        },
+        descricao: {
+            type: DataTypes.STRING(1024),
+            allowNull: false
+        },
+        pagamento: {
+            type: DataTypes.DECIMAL(),
+            allowNull: false
+        }
+    },
+        {
+            createdAt: 'criado_em',
+            updatedAt: 'atualizado_em',
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+            freezeTableName: true
+        });
+    return Proposta;
+}
