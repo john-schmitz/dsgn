442a33e96c22a04638299ef7335e91be2459d124
Treeek <johngc2010@hotmail.com>
2018-10-10 19:18:59 -0300

    add models no Sequelize

--- back/config/db.js
+++ back/config/db.js
@@ -7,11 +7,11 @@ const sequelize = new Sequelize("mysql://root:123456@localhost:3306/dsgn", {
 
 sequelize.authenticate().then(() => {
 	// eslint-disable-next-line
-    console.log("Banco Conectado");
+	console.log("Banco Conectado");
 
 }).catch(err => {
 	// eslint-disable-next-line
-    console.log(err);
+	console.log(err);
 });
 
 const db = {};
@@ -19,7 +19,14 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-
+db.area = require("../model/area")(sequelize, Sequelize);
+db.cliente = require("../model/cliente")(sequelize, Sequelize);
 db.designer = require("../model/designer")(sequelize, Sequelize);
+db.imagemPortifolio = require("../model/imagemPortifolio")(sequelize, Sequelize);
+db.mensagem = require("../model/mensagem")(sequelize, Sequelize);
+db.moderador = require("../model/moderador")(sequelize, Sequelize);
+db.projeto = require("../model/projeto")(sequelize, Sequelize);
+db.proposta = require("../model/proposta")(sequelize, Sequelize);
+db.versao = require("../model/versao")(sequelize, Sequelize);
 
 module.exports = db;
